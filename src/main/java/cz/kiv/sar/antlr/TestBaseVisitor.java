0c21fd0ecb05fd30d889c0f88c09044f5771495a
Prema <kouba.prem@gmail.com>
2021-12-08 21:24:20 +0100

    testing antlr plugin for idea

---
+++ src/main/java/cz/kiv/sar/antlr/TestBaseVisitor.java
@@ -0,0 +1,28 @@
+// Generated from C:/git/DBDtoSQL/grammar\Test.g4 by ANTLR 4.9.2
+package cz.kiv.sar.antlr;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link TestVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public class TestBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements TestVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitMain(TestParser.MainContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitName(TestParser.NameContext ctx) { return visitChildren(ctx); }
+}
